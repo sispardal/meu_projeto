55066f68c6ac5b06abf5bc52580df196f40fa3bf
Jose Cruz <josecruz7oficial@gmail.com>
2026-04-22 21:37:22 -0300

    teste rota funcionando

--- core/views.py
+++ core/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
-# Create your views here.
+    return HttpResponse("SITE ONLINE 🚀")
