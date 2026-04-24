45dca3382f56152a3945b4c56c12e60f708d9649
Jose Cruz <josecruz7oficial@gmail.com>
2026-04-24 02:54:59 -0300

    test database

--- config/settings.py
+++ config/settings.py
@@ -72,9 +72,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    )
+    'default': dj_database_url.config()
 }
 
 
